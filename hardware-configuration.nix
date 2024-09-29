25a0666c200c5cf3922d942a21742ed944ca733b
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2024-09-29 14:52:31 -0400

    feat(hardware): add swapfile

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -42,7 +42,10 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+	device = "/var/lib/swapfile";
+	size = 16*1024; 
+  } ];
 
   # Enables DHCP on each ethernet and wireless interface.
   # In case of scripted networking (the default),
